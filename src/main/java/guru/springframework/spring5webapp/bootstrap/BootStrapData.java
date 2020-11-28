2287900ed3920f65bb31112510f387892bb77dd3
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-11-28 17:54:42 +0600

    Publisher relationships

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -40,19 +40,28 @@ public class BootStrapData implements CommandLineRunner {
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "23437686");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
         System.out.println("Publisher Count: " + publisherRepository.count());
+        System.out.println("Publisher number of books: " + publisher.getBooks().size());
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -16,6 +16,10 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
     public Publisher() {
     }
 
@@ -73,6 +77,14 @@ public class Publisher {
 
     public void setZip(String zip) {
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     @Override
